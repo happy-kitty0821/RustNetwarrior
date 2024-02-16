3af95c3feb7fa0561cbb33cc4cdef9da8d2a8ae4
happy-kitty0821 <ayushhaang09@gmail.com>
2024-02-16 21:53:04 +0500

    first rust program

---
+++ Day1/first.rs
@@ -0,0 +1,11 @@
+// This is a comment, and is ignored by the compiler.
+// This is the main function.
+fn main() {//in rust the main function is the entrypoint of the program 
+    //to define a fucntion we use the fn keyword example: fn testFunction(){}
+    // Statements here are executed when the compiled binary is called.
+    // Print text to the console.
+    println!("Hello World!");//the println keyword will make a new line
+    print!("Hellow World this is using the print keyword"); //in rust we need to use semicolon to terminate a line
+}
+
+//to compile we use the rustc first.rs
